9dd53545238e528bfb3d9badd273625d0c557662
Niharika-G <40772852+Niharika-G@users.noreply.github.com>
2018-07-06 04:22:04 -0500

    Added Download option
    Added Data Dictionary Download Button to the webpage with the “datadictionary_fraud_freetrial.csv”  file

--- Amruta01_Dash/app.py
+++ Amruta01_Dash/app.py
@@ -1,9 +1,7 @@
 import base64
-import datetime
 import io
 
 import flask
-
 import dash
 # from dash import Dash
 from dash.dependencies import Input, Output
@@ -12,21 +10,24 @@ import dash_html_components as html
 import dash_table_experiments as dt
 import numpy as np
 import pandas as pd
-import xlwt
+# import xlwt
+import os
 
 app = dash.Dash()
+app.config['UPLOAD_FOLDER'] = "static"
 server = app.server
-app.title='AmrutaInc'
+app.title = 'AmrutaInc'
 app.scripts.config.serve_locally = True
 
 app.layout = html.Div([
 
         html.A(html.Img(src="/static/amruta_logo.jpg", alt='Amruta Inc',
-        style={
+                        style={
 
-            'width': '30%',
-            'height': '100px'
-            }), href='http://amrutainc.com/'),
+                                'width': '30%',
+                                'height': '100px'
+                                }
+                        ), href='http://amrutainc.com/'),
 
             html.H2('GEICO'),
 
@@ -45,11 +46,33 @@ app.layout = html.Div([
             'borderStyle': 'solid',
             'borderRadius': '5px',
             'textAlign': 'center',
-            'margin': '10px'
+            'margin': '10px',
+            'float': 'left'
         },
         # Allow multiple files to be uploaded
         multiple=True
-    ),
+                ),
+
+    html.A(html.Button('Download',
+        style={
+
+                'width': '30%',
+                'height': '62px',
+                'lineHeight': '60px',
+                'borderWidth': '1px',
+                'borderStyle': 'solid',
+                'borderRadius': '5px',
+                'textAlign': 'center',
+                'margin': '10px',
+                'float': 'left'
+                },),
+                id='download-link',
+                download="Fraud_Data.csv",
+                href="",
+                target="_blank",
+                n_clicks=0
+        ),
+
     html.Div(id='output-data-upload'),
     html.Div(dt.DataTable(rows=[{}]), style={'display': 'none'})
 
@@ -67,9 +90,8 @@ def parse_contents(contents, filename, date):
                 io.StringIO(decoded.decode('utf-8')))
         elif 'xls' in filename:
             # Assume that the user uploaded an excel file
-
             df = pd.read_excel(io.BytesIO(decoded))
-            df['Score'] = np.random.randint(1,1000,size=len(df))
+            df['Score'] = np.random.randint(1, 1000, size=len(df))
 
     except Exception as e:
         print(e)
@@ -81,11 +103,11 @@ def parse_contents(contents, filename, date):
         html.H5(filename),
         # html.H6(datetime.datetime.fromtimestamp(date)),
 
-        dt.DataTable(rows=df.to_dict(orient='records'),row_selectable=True,),
+        dt.DataTable(rows=df.to_dict(orient='records'), row_selectable=True,),
             html.P('Do you agree?',
-            style={
-                'textAlign': 'center',
-    }),
+                    style={
+                            'textAlign': 'center',
+                        }),
             dcc.RadioItems(
             options=[
                 {'label': 'Yes', 'value': 'Yes'},
@@ -94,7 +116,7 @@ def parse_contents(contents, filename, date):
             value='No',
             style={
                 'textAlign': 'center',
-    }
+                    }
         )
 
         # For debugging, display the raw contents provided by the web browser
@@ -104,8 +126,6 @@ def parse_contents(contents, filename, date):
             #'wordBreak': 'break-all'
         #})
     ])
-
-
 
 @app.callback(Output('output-data-upload', 'children'),
               [Input('upload-data', 'contents'),
@@ -127,6 +147,21 @@ app.css.append_css({
 def favicon():
     return flask.send_from_directory(os.path.join(server.root_path, 'static'),
                                      'Robsonbillponte-Sinem-File-Downloads.ico')
+
+@app.callback(
+    Output('download-link', 'href'),
+    [Input('download-link', 'n_clicks')])
+
+def generate_report_url(n_clicks):
+
+    return '/dash/urldownload'
+
+@app.server.route('/dash/urldownload')
+
+def generate_report_url():
+
+    return flask.send_file('Fraud_Data.csv', attachment_filename='Fraud_Data.csv', as_attachment=True)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
